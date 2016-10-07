c2f58f5c38587f34a08a5bcad159e6b6c4ed47ec
joe820730 <joe.tsai8207@gmail.com>
2016-10-07 23:53:51 +0800

    Use UBERTC's toolchain.

--- build_kernel.sh
+++ build_kernel.sh
@@ -6,7 +6,8 @@ export ARCH=arm
 #export CROSS_COMPILE=/home/simin/gcc-linaro-arm-linux-gnueabihf-4.8-2014.04_linux/bin/arm-linux-gnueabihf-
 #export CROSS_COMPILE=/home/simin/gcctools/gcc-a15/arm-cortex_a15-linux-gnueabihf-linaro_4.9/bin/arm-cortex_a15-linux-gnueabihf-
 #export CROSS_COMPILE=/home/joe/toolchain/arm-cortex_a15-linux-gnueabihf-linaro_4.9.4-2015.06/bin/arm-cortex_a15-linux-gnueabihf-
-export CROSS_COMPILE=/opt/linaro/gcc-linaro-5.3-2016.02-x86_64_arm-linux-gnueabihf/bin/arm-linux-gnueabihf-
+#export CROSS_COMPILE=/opt/linaro/gcc-linaro-5.3-2016.02-x86_64_arm-linux-gnueabihf/bin/arm-linux-gnueabihf-
+export CROSS_COMPILE=/mnt/sdc6/cm11/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-5.3/bin/arm-linux-androideabi-
 
 #export CROSS_COMPILE=/home/simin/gcc-linaro-arm-linux-gnueabihf-4.9-2014.09_linux/bin/arm-linux-gnueabihf-
 
